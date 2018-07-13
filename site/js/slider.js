13d0f6cbff42f0741a72d1f412567fabc307afa7
MadMaxInsight <38112884+MadMaxInsight@users.noreply.github.com>
2018-07-13 22:55:24 +0300

    мобильная версия

--- site/js/slider.js
+++ site/js/slider.js
@@ -96,3 +96,25 @@ $('.primary_buttons_call').click(function(){
 $('.close').click(function(){
   $('.popup').toggle();
 });
+
+$('#nav_invisible>.nav-items>.nav-item').click(function(){
+  $('#nav_invisible').toggle();
+  $('.top_button').toggleClass('top_button_active');
+
+});
+
+// $('.nav-item>a').click(function(){
+// // $("#top_nav").on("click","a", function (event) {
+//   //отменяем стандартную обработку нажатия по ссылке
+//   event.preventDefault();
+
+//   //забираем идентификатор бока с атрибута href
+//   var id  = $(this).attr('href'),
+
+//   //узнаем высоту от начала страницы до блока на который ссылается якорь
+//     top = $(id).offset().top;
+  
+//   //анимируем переход на расстояние - top за 1500 мс
+//   $('body,html').animate({scrollTop: "300px" }, 1500);
+//   // $('body,html').scrollTop(top);
+// });
