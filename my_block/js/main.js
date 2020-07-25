0abcc3ada4c61a3918496a5fa3cf60722c7dfdbe
MadMaxInsight <maxvarvarin3@gmail.com>
2020-07-26 00:22:25 +0300

    final

--- my_block/js/main.js
+++ my_block/js/main.js
@@ -3,21 +3,23 @@ const second = 1000,
   hour = minute * 60,
   day = hour * 24;
 
-let countDown = new Date("Sep 30, 2020 00:00:00").getTime(),
+let countDown = new Date("Aug 5, 2020 22:00:00").getTime(),
+  // let countDown = new Date("Jul 27, 2020 00:00:00").getTime(),
   x = setInterval(function() {
     let now = new Date().getTime(),
       distance = countDown - now;
 
-    // (document.getElementById("days").innerText = Math.floor(distance / day)),
-    (document.getElementById("hours").innerText = Math.floor(
-      (distance % day) / hour
-    )),
+    (document.getElementById("days").innerText = Math.floor(distance / day)),
+      (document.getElementById("hours").innerText = Math.floor(
+        (distance % day) / hour
+        // distance / hour
+      )),
       (document.getElementById("minutes").innerText = Math.floor(
         (distance % hour) / minute
-      )),
-      (document.getElementById("seconds").innerText = Math.floor(
-        (distance % minute) / second
       ));
+    // (document.getElementById("seconds").innerText = Math.floor(
+    //   (distance % minute) / second
+    // ));
 
     //do something later when date is reached
     //if (distance < 0) {
