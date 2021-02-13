3668876a7961dcb3231449ef9adb9c5cf751c07a
MadMaxInsight <maxvarvarin3@gmail.com>
2021-02-13 15:44:12 +0300

    UPD

---
+++ jalousie/js/slick-controll.js
@@ -0,0 +1,10 @@
+jQuery.noConflict();
+
+jQuery(document).ready(function(){
+      $('.slider-slick').slick({
+      infinite: true,
+      dots: true,
+      slidesToShow: 3,
+      slidesToScroll: 1
+    });
+    });
